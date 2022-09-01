fbb18d0c21824d6705c6c0111a6613456c78c525
f <f.farokhifar987@gmail.com>
2022-09-02 00:51:37 +0430

    add Carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,23 +1,23 @@
 import { useEffect, useRef, useState } from "react";
 import { MdChevronRight, MdOutlineChevronLeft } from "react-icons/md";
-const featuredProducts = [
+const featuredImages = [
   {
     image:
-      "https://images.unsplash.com/photo-1599707367072-cd6ada2bc375?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1333&q=80",
+      "https://images.unsplash.com/photo-1542990254-85e6a9a2ef92?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
     title: "lorem1",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
   },
   {
     image:
-      "https://images.unsplash.com/photo-1588444650700-fd887f15a9e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+      "https://images.unsplash.com/photo-1515562141207-7a88fb7ce338?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
     title: "lorem2",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
   },
   {
     image:
       "https://images.unsplash.com/photo-1592317295760-5c1f677dfc78?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1915&q=80",
     title: "lorem3",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
   },
 ];
 let count = 0;
@@ -47,35 +47,35 @@ const Carousel = () => {
     clearInterval(sliderInterval);
   };
   const handleOnNextClick = () => {
-    count = (count + 1) % featuredProducts.length;
+    count = (count + 1) % featuredImages.length;
     setCurrentIndex(count);
     slideRef.current.classList.add("fade-anim");
   };
   const handleOnPrevClick = () => {
-    const productsLength = featuredProducts.length;
+    const productsLength = featuredImages.length;
     count = (currentIndex + productsLength - 1) % productsLength;
     setCurrentIndex(count);
     slideRef.current.classList.add("fade-anim");
   };
   return (
-    <div className="container mx-auto mt-20">
+    <div className="container mx-auto mt-20 ">
       <section
         ref={slideRef}
-        className="w-full select-none mx-auto h-[32rem] flex flex-col "
+        className="w-full select-none mx-auto h-[32rem] flex flex-col  p-6 "
       >
-        <div className="w-full h-full p-4  mx-auto ">
+        <div className="w-full h-full  mx-auto">
           <div
             className="h-full flex flex-col items-left justify-between bg-white shadow-lg hover:shadow-xl rounded overflow-hidden   bg-cover bg-center"
             style={{
-              backgroundImage: `url(${featuredProducts[currentIndex].image})`,
+              backgroundImage: `url(${featuredImages[currentIndex].image})`,
             }}
           >
-            <div className=" h-3/4 p-20 flex flex-col items-left justify-between">
-              <h5 className="text-white text-2xl leading-tight font-bold">
-                {featuredProducts[currentIndex].title}
+            <div className="backdrop-blur-md bg-white/30 h-3/4 p-4 sm:p-6 md:p-8 lg:p-10 xl:p-10 flex flex-col items-center justify-between m-10 ">
+              <h5 className="text-amber-400 text-2xl leading-tight font-bold">
+                {featuredImages[currentIndex].title}
               </h5>
-              <p className="text-white text-medium text-lg">
-                {featuredProducts[currentIndex].desc}
+              <p className="text-white text-medium text-lg text-center">
+                {featuredImages[currentIndex].desc}
               </p>
               <div>
                 <button
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -16,41 +16,42 @@ const ProductList = () => {
   };
   return (
     <Layout>
-    <main className="container mx-auto ">
-      <section className=" flex flex-wrap p-6 items-center justify-between ">
-        {products.map((product) => {
-          return (
-            <div
-              key={product.id}
-              className="flex flex-col justify-center items-center sm:w-1/3 md:w-1/3 xl:w-1/4 p-4 my-8 mx-auto w-full"
-            >
-              <img
-                src={`${product.image}`}
-                className="bg-gray-300 h-64 w-full rounded-t-lg shadow-md object-fit bg-center "
-              alt={product.name}/>
+      <main className="container mx-auto ">
+        <section className=" flex flex-wrap p-6 items-center justify-between ">
+          {products.map((product) => {
+            return (
+              <div
+                key={product.id}
+                className="flex flex-col justify-center items-center sm:w-1/3 md:w-1/3 xl:w-1/4 p-4 my-8 mx-auto w-full"
+              >
+                <img
+                  src={`${product.image}`}
+                  className="bg-gray-300 h-64 w-full rounded-t-lg shadow-md object-fit bg-center "
+                  alt={product.name}
+                />
 
-              <div className="w-full  bg-white overflow-hidden rounded-b-lg">
-                <div className="py-2 text-center font-bold uppercase tracking-wide text-gray-800">
-                  {product.name}
-                </div>
-                <div className="flex items-center justify-between py-2 px-3 bg-amber-400">
-                  <h1 className="text-gray-800 font-bold ">
-                    ${product.offPrice}
-                  </h1>
-                  <BiCartAlt
-                    size={30}
-                    className={`cursor-pointer ${
-                      checkInCart(cart, product)
-                        ? "text-green-600"
-                        : "text-white"
-                    }`}
-                    onClick={() => addProductHandler(product)}
-                  />
+                <div className="w-full  bg-white overflow-hidden rounded-b-lg">
+                  <div className="py-2 text-center font-bold uppercase tracking-wide text-gray-800">
+                    {product.name}
+                  </div>
+                  <div className="flex items-center justify-between py-2 px-3 bg-amber-400">
+                    <h1 className="text-gray-800 font-bold ">
+                      ${product.offPrice}
+                    </h1>
+                    <BiCartAlt
+                      size={30}
+                      className={`cursor-pointer ${
+                        checkInCart(cart, product)
+                          ? "text-green-600"
+                          : "text-white"
+                      }`}
+                      onClick={() => addProductHandler(product)}
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
         </section>
       </main>
     </Layout>
