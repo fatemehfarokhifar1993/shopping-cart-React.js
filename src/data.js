b89c37ac58a827166c99a083e05422d5e5c99b79
f <f.farokhifar987@gmail.com>
2022-09-01 16:59:38 +0430

    change date

--- src/data.js
+++ src/data.js
@@ -3,93 +3,127 @@
 export const products = [
   {
     id: 1,
-    name: "Winter body",
-    description: [
-      { support: "گارانتی مادام العمر" },
-      { support: "ارسال رایگان" },
-      { support: "اورجینال" },
-    ],
+    name: "lorem 1",
     price: 120,
     offPrice: 120,
     discount: 0,
-    image: "https://s4.uupload.ir/files/item1_soj.jpg",
+    image:
+    "https://images.unsplash.com/photo-1603561596112-0a132b757442?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
   },
   {
     id: 2,
-    name: "Adidas",
-    description: [{ support: "گارانتی مادام العمر" }, { support: "اورجینال" }],
+    name: "lorem 2",
     price: 110,
     offPrice: 100,
     discount: 8,
-    image: "https://s4.uupload.ir/files/item2_fc5s.jpg",
+    image:
+    "https://images.unsplash.com/photo-1586104195538-050b9f74f58e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
   },
   {
     id: 3,
-    name: "Vans",
-    description: [
-      { support: "گارانتی مادام العمر" },
-      { support: "اورجینال" },
-      { support: "ارسال رایگان" },
-    ],
+    name: "lorem 3",
     price: 99,
     offPrice: 89,
     discount: 10,
-    image: "https://s4.uupload.ir/files/item3_zvc9.jpg",
+    image:
+    "https://images.unsplash.com/photo-1629224316810-9d8805b95e76?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
   },
   {
     id: 4,
-    name: "White",
-    description: [
-      { support: "گارانتی مادام العمر" },
-      { support: "اورجینال" },
-      { support: "ارسال رایگان" },
-    ],
+    name: "lorem 4",
     price: 260,
     offPrice: 220,
     discount: 15,
-    image: "https://s4.uupload.ir/files/item4_zr94.jpg",
+    image:
+    "https://images.unsplash.com/photo-1589128777073-263566ae5e4d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
   },
   {
     id: 5,
-    name: "Croopped-shoe",
-    description: [
-      { support: "گارانتی مادام العمر" },
-      { support: "اورجینال" },
-      { support: "ارسال رایگان" },
-    ],
+    name: "lorem 5",
     price: 150,
     offPrice: 150,
     discount: 0,
-    image: "https://s4.uupload.ir/files/item5_24ye.jpg",
+    image:
+    "https://images.unsplash.com/photo-1619119069152-a2b331eb392a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
   },
   {
     id: 6,
-    name: "Blues",
-    description: [
-      { support: "گارانتی مادام العمر" },
-      { support: "اورجینال" },
-      { support: "ارسال رایگان" },
-    ],
+    name: "lorem 6",
     price: 220,
     offPrice: 200,
     discount: 13,
-    image: "https://s4.uupload.ir/files/item6_7jfy.jpg",
+    image:
+    "https://images.unsplash.com/photo-1598560917807-1bae44bd2be8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
   },
+  {
+    id: 7,
+    name: "lorem 7",
+    price: 120,
+    offPrice: 120,
+    discount: 0,
+    image:
+    "https://images.unsplash.com/photo-1588891825655-aa526b56f805?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+  {
+    id: 8,
+    name: "lorem 8",
+    price: 110,
+    offPrice: 100,
+    discount: 8,
+    image:
+    "https://images.unsplash.com/photo-1586104237516-5b7075e00d45?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+  {
+    id: 9,
+    name: "lorem 9",
+    price: 99,
+    offPrice: 89,
+    discount: 10,
+    image:
+    "https://images.unsplash.com/photo-1602751584581-2e0372975b46?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1077&q=80",
+  },
+  {
+    id:10,
+    name: "lorem 10",
+    price: 260,
+    offPrice: 220,
+    discount: 15,
+    image:
+    "https://images.unsplash.com/photo-1636697005346-23d9c0fd53fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80",
+  },
+  {
+    id: 11,
+    name: "lorem 11",
+    price: 150,
+    offPrice: 150,
+    discount: 0,
+    image:
+    "https://images.unsplash.com/photo-1602752250055-5ebb552fc3ae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+  {
+    id: 12,
+    name: "lorem 12",
+    price: 220,
+    offPrice: 200,
+    discount: 13,
+    image:
+    "https://images.unsplash.com/photo-1622569393438-a875792bd3d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
+  },{
+    id: 13,
+    name: "lorem 13",
+    price: 150,
+    offPrice: 150,
+    discount: 0,
+    image:
+    "https://images.unsplash.com/photo-1615655114865-4cc1bda5901e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+  },
+  {
+    id: 14,
+    name: "lorem 14",
+    price: 220,
+    offPrice: 200,
+    discount: 13,
+    image:
+    "https://images.unsplash.com/photo-1598560917505-59a3ad559071?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+  }
 ];
-
-/* export const users = [
-  {
-    name: "Saheb mohamadi",
-    email: "saheb.ex@gmail.com",
-    password: bcrypt.hashSync("12345", 8),
-    phoneNumber: "09180000000",
-    isAdmin: true,
-  },
-  {
-    name: "John",
-    email: "user@example.com",
-    password: bcrypt.hashSync("1234", 8),
-    isAdmin: false,
-    phoneNumber: "09181230000",
-  },
-]; */
