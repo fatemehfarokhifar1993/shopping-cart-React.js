453e22a1b93fe9d8a63a3acc1bc41a5d80fbdbc5
f <f.farokhifar987@gmail.com>
2022-09-01 19:19:42 +0430

    productlist with tailwindcss

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -5,6 +5,7 @@ const Layout = ({ children }) => {
     <div>
       <Navigation />
       {children}
+
     </div>
   );
 };
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,54 +1,11 @@
-import Layout from "../../Layout/Layout";
-import * as data from "../../data.js";
-import { useCart, useCartActions } from "../../Providers/CartProvider";
-import "./HomePage.css";
-import { toast } from "react-toastify";
-function checkInCart(cart, product) {
-  return cart.find((c) => c.id === product.id);
-}
-const HomePage = () => {
-  const { cart } = useCart();
 
-  const dispatch = useCartActions();
-  const addProductHandler = (product) => {
-    toast.success(`${product.name} added to cart ! `);
-    dispatch({ type: "ADD-TO-CART", payload: product });
-  };
+import ProductList from "../../components/ProductList";
+import Layout from "../../Layout/Layout";
+
+const HomePage = () => {
   return (
     <Layout>
-      <main className="bd-container">
-        <section className=" productList">
-          {data.products.map((product) => {
-            return (
-              <div className="product" key={product.id}>
-                <div className="productImg">
-                  <img src={product.image} alt={product.name} />
-                </div>
-                <div className="productDesc">
-                  <h3 className="productDesc-title">{product.name}</h3>
-                  <div className="productDesc-price-offprice">
-                    <h3 className={`${product.discount && "productDesc-offprice"}`}>
-                      {product.discount ? product.offPrice : product.price} $
-                    </h3>
-                    <h4
-                      className={`${product.discount && "productDesc-price"}`}
-                    >
-                      {product.discount ? product.price :<div style={{visibility:"hidden"}}>0</div>}
-                    </h4>
-                  </div>
-                </div>
-                <button
-                  onClick={() => addProductHandler(product)}
-                  className={`btn ${checkInCart(cart, product) && "btn-incart"}`}
-                >
-                  {checkInCart(cart, product) ? "In Cart" : "Add to Cart"}
-                </button>
-                <div className={`${product.discount ?"product-discount":"none"}`}>{product.discount}%</div>
-              </div>
-            );
-          })}
-        </section>
-      </main>
+     <ProductList/>
     </Layout>
   );
 };
